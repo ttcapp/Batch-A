3b5a504e0e11ec8f31c9fdc294715baaf3d1141e
jannatsuha242424@gmail.com <24101997Jannat>
2021-11-03 09:35:13 +0600

    1st commit, 3 November

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp( MyApp());
 }
+
+double conHeight=100;
+double conWidth=100;
+
+String imgSrc1="https://p0.pxfuel.com/preview/853/319/641/flower-nature-summer-plant.jpg";
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,6 +20,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -24,13 +31,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    double height= MediaQuery.of(context).size.height;
+    double width= MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: Text("My AppBar"),
+        title: Text("My App Bar"),
+      ),
+      body: Container(
+
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              InkWell(
+                onTap: (){
+                  setState(() {
+                    conHeight=300;
+                    conWidth=300;
+                  });
+                },
+                onDoubleTap: (){
+                  setState(() {
+                    conHeight=100;
+                    conWidth=100;
+                  });
+                },
+                child: AnimatedContainer(
+                  height: conHeight,
+                  width: conWidth,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    image: DecorationImage(
+                        image: AssetImage("assets/image/img2.jpg"),
+                      fit: BoxFit.cover
+
+                    ),
+                    borderRadius: BorderRadius.circular(180),
+                    border: Border.all(color: Colors.purple,width: 5)
+                  ),
+                  duration: Duration(milliseconds: 4000),
+                ),
+              ),
+              Image.asset("assets/image/img3.png"),
+              Image.asset("assets/image/img2.jpg"),
+              Image.asset("assets/image/img3.png"),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
 
 
